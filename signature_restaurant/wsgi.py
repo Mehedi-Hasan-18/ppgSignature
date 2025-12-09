2693bf1cc65ccbb88b105fb4aa5cbe15fa2e3e83
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-12-10 02:16:15 +0800

    Fix WSGI application import for Vercel runtime detection

--- signature_restaurant/wsgi.py
+++ signature_restaurant/wsgi.py
@@ -9,4 +9,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signature_restaurant.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+app = get_wsgi_application()
