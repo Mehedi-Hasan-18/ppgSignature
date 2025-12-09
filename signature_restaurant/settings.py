146fa0ecc8b2e0612e342c914b73c8b77faf000b
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-12-10 00:46:20 +0800

    setup supabase

--- signature_restaurant/settings.py
+++ signature_restaurant/settings.py
@@ -5,6 +5,7 @@ Django settings for signature_restaurant project.
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from decouple import config
 
 load_dotenv()
 
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'signature_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname'),
+        'USER': config('user'),
+        'PASSWORD': config('password'),
+        'HOST': config('host'),
+        'PORT': config('port')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
